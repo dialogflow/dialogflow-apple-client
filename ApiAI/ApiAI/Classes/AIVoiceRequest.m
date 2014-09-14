27ba1ca8b1ac5295176746f91c1b61d66d563f74
Dmitriy Kuragin <kuragin@speaktoit.com>
2014-09-14 23:56:30 +0700

    added asrPref parameter

--- ApiAI/ApiAI/Classes/AIVoiceRequest.m
+++ ApiAI/ApiAI/Classes/AIVoiceRequest.m
@@ -115,7 +115,7 @@
     NSMutableData *data = [[[NSString stringWithFormat:@"--%@\r\n", _boundary] dataUsingEncoding:NSUTF8StringEncoding] mutableCopy];
     [data appendData:[@"Content-Disposition: form-data; name=\"request\"; filename=\"request.json\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
     [data appendData:[@"Content-Type: application/json\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    [data appendData:[@"{\"agent_id\":\"test\"}" dataUsingEncoding:NSUTF8StringEncoding]];
+    [data appendData:[@"{\"asrPref\":\"stiv2\"}" dataUsingEncoding:NSUTF8StringEncoding]];
 
     [data appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", _boundary] dataUsingEncoding:NSUTF8StringEncoding]];
     [data appendData:[@"Content-Disposition: form-data; name=\"voiceData\"; filename=\"qwe.wav\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
